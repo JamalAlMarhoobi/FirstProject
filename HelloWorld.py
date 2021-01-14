7f6798f36acc1f238d7a0bcf7d25e25bfbb25fef
Jamal Al Marhoobi <WJIS237@live.rhul.ac.uk>
2021-01-14 10:53:09 +0000

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,7 +2,7 @@
 # Copy the template below and add your names
 # **********************************
 # Add your name as a comment here
-print("Your name here, this is a new version")
+print("Your name here, Jamal")
 # **********************************
 # Do not remove/delete previous comments or line, just add yours afterwards
 # Start adding your names from this point on
